1ffc6cfff6de2effa869accaf40884c89c0ae276
Killian MOTEL <killian.motel@gmail.com>
2022-05-18 15:24:56 +0200

    first datas

---
+++ Source/UMad/UMadGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UMadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMAD_API AUMadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
